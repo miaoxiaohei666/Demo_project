529b4e504327473c7294585b45702e323ed57d6e
miaoxiaohei <miaoxiaohei666@gmail.com>
2022-06-23 17:12:53 +0800

    6.24

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("xxy12345678");
+        System.out.println("xxy12345678");
         System.out.println( "Hello World!" );
     }
 }
