a4a5d427cb6990b9b99bfb76bd868b462432fd82
miaoxiaohei <miaoxiaohei666@gmail.com>
2022-06-23 19:08:10 +0800

    my try

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("xxy12345678");
+        System.out.println("xxy1");
         System.out.println( "Hello World!" );
     }
 }
